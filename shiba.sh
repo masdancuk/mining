1505ff6d20b84619d235cc823ee3e4046dc83736
masdancuk <89745118+masdancuk@users.noreply.github.com>
2021-08-30 17:16:33 +0700

    Add files via upload

---
+++ shiba.sh
@@ -0,0 +1,7 @@
+! sudo apt update
+! wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+! tar -xvf xmrig-6.13.1-linux-x64.tar.gz
+! ./xmrig-6.13.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x21ff67ab8084ee0b7ed7f6a5f41551aa0c5f8b16.hexcal -p x --threads=6
+
+
+
